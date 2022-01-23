ee85bff638447476506e987a0c04fb54ba59dc12
Luis Gómez <55402700+luisjedev@users.noreply.github.com>
2022-01-23 20:05:30 +0100

    twitter

--- envylostark/components/Footer.js
+++ envylostark/components/Footer.js
@@ -18,7 +18,7 @@ export const Footer = () => {
             <span className="ml-3 text-xl">ENVY</span>
             </a>
             <p className="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">©{new Date().getFullYear()} —
-            <a href="https://twitter.com/knyttneve" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@envyclub</a>
+            <a href="https://twitter.com/Envy_LostArk" className="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@envyclub</a>
             </p>
             <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
             <a className="text-gray-400">
