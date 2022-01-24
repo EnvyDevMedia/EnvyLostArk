26fedec074256a1f21a1adf0c1d3c10be7ec51ad
Han <hander.juan@gmail.com>
2022-01-24 10:52:33 +0200

    pwa

--- envylostark/next.config.js
+++ envylostark/next.config.js
@@ -5,3 +5,14 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
 }
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
