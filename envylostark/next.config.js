73c1c3615ba1839914c6588785390fe877dfed9e
Han <hander.juan@gmail.com>
2022-01-24 10:58:52 +0200

    config

--- envylostark/next.config.js
+++ envylostark/next.config.js
@@ -1,11 +1,3 @@
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-}
-
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
